86e7e1e76e232f14179297966987d0272fa0784e
rgzrgzm <mariomkt97@gmail.com>
2024-04-13 13:45:59 -0700

    add cors in headers

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,14 @@ async function getData() {
   const html = await getHTML(url);
   const data = processHTML(html);
 
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify({
       response: data,
     }),
