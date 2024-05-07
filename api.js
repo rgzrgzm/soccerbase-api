7c5cb169cc6e0974dea6af7576290371c4e7746b
rgzrgzm <mariomkt97@gmail.com>
2024-05-06 19:54:51 -0700

    added routing

--- api.js
+++ api.js
@@ -1,18 +1,31 @@
-const { getData } = require('./controllers');
+const { getData } = require("./controllers");
 
 exports.handler = async (event, context) => {
-    if (event.httpMethod === "GET") {
-        try {
-          return await getData()
-        } catch (error) {
-            // Return an error response if there was an issue processing the request
-            return {
-                statusCode: 500,
-                body: JSON.stringify({
-                    error: "Failed to process GET request",
-                    error: error.message,
-                }),
-            };
-        }
+  if (event.httpMethod === "GET") {
+    const path = event.path;
+    const origin_url = process.env.ORIGIN_URL;
+    try {
+      if (path === `${origin_url}/leagues_list`) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            msg: "Leagues list coming soon",
+          }),
+        };
+      }
+
+      if (path === origin_url) {
+        return await getData();
+      }
+    } catch (error) {
+      // Return an error response if there was an issue processing the request
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: "Failed to process GET request",
+          error: error.message,
+        }),
+      };
     }
+  }
 };
