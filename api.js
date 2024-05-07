6f35e2f322029e434650e4332628b0ae61f06266
rgzrgzm <mariomkt97@gmail.com>
2024-05-06 19:02:27 -0700

    add new route

--- api.js
+++ api.js
@@ -1,18 +1,31 @@
-const { getData } = require('./controllers');
+const { getData } = require("./controllers");
 
 exports.handler = async (event, context) => {
-    if (event.httpMethod === "GET") {
-        try {
-          return await getData()
-        } catch (error) {
-            // Return an error response if there was an issue processing the request
-            return {
-                statusCode: 500,
-                body: JSON.stringify({
-                    error: "Failed to process GET request",
-                    error: error.message,
-                }),
-            };
-        }
+  if (event.httpMethod === "GET") {
+    const path = event.path;
+
+    try {
+      if (path === "/") {
+        return await getData();
+      }
+
+      if (path === "/leagues") {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            msg: "Leagues list",
+          }),
+        };
+      }
+    } catch (error) {
+      // Return an error response if there was an issue processing the request
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: "Failed to process GET request",
+          error: error.message,
+        }),
+      };
     }
+  }
 };
